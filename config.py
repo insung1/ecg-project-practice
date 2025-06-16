f82c8e6e3d233d46de8d76e68984f44df3b58987
insung1 <insys123@naver.com>
2025-06-16 11:09:34 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# config.py
+
+BASE_PATH = "C:/ecgpy/proceed/processed"
+CHECKPOINT_DIR = "checkpoints"
+HISTORY_PATH = "history.json"
+
+VALID_PATIENT_IDS = [
+    100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
+    111, 112, 113, 114, 115, 116, 117, 118, 119,
+    121, 122, 123, 124,
+    200, 201, 202, 203, 205, 207, 208, 209,
+    210, 212, 213, 214, 215, 217, 219, 220,
+    221, 222, 223, 228, 230, 231, 232, 233, 234
+]
+
+NUM_EPOCHS = 20
+BATCH_SIZE = 32
+LEARNING_RATE = 0.001
